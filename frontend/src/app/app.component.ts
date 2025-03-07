b669b347d2a136bdfa43d985dc215f2e87c448a5
Lrigami <prune.thiry@hotmail.com>
2025-03-07 12:41:45 +0100

    feat(task-card): test task-card component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './components/buttons/button.component';
+import { TaskCardComponent } from './components/cards/task-card/task-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent],
+  imports: [RouterOutlet, ButtonComponent, TaskCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
