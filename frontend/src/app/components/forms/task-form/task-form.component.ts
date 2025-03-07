6fb9d1d2c150f1829f84e7ab58431649003d4ff4
Lrigami <prune.thiry@hotmail.com>
2025-03-07 18:08:56 +0100

    feat(task-form): create task-form structure

--- frontend/src/app/components/forms/task-form/task-form.component.ts
+++ frontend/src/app/components/forms/task-form/task-form.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { ButtonComponent } from '../../buttons/button.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'task-form',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
 export class TaskFormComponent {
+  @Output() isFormVisible = new EventEmitter<any>();
 
+  taskForm = new FormGroup({
+    title: new FormControl('', Validators.required), // Champ obligatoire
+  });
+
+  closeForm() {
+    this.isFormVisible.emit(false);
+  }
+
+  openNewTagForm() {
+    
+  }
 }
