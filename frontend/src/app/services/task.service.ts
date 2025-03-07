1426d066e516970c0ef9d2df1a29a4fd6e26b69d
Lrigami <prune.thiry@hotmail.com>
2025-03-07 12:40:41 +0100

    style: convert js to ts

---
+++ frontend/src/app/services/task.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class TaskService {
+    private taskListSubject = new BehaviorSubject<void>(null!);
+    taskList$ = this.taskListSubject.asObservable();
+    private apiUrl = 'http://localhost:3000/tasks';
+
+    constructor(private http: HttpClient) {}
+
+    // Est-ce que ça suffit ou est-ce que s'il manque un paramètre, cela posera problème pour la création de données dans le back ? 
+    createTask(task: {title: string, description?: string, dueDate?: Date, priority?: string, kanban?: string, done?: boolean}): Observable<any> {
+        return this.http.post<any>(this.apiUrl, task);
+    }
+
+    checkTask(task: {id: number, done: boolean}) {
+        return this.http.put<any>(`${this.apiUrl}/${task.id}`, task).pipe(
+            tap(() => {
+                this.taskListSubject.next();
+            })
+        )
+    }
+
+    updateTask(task: {id: number, title: string, description?: string, dueDate?: Date, priority?: string, kanban?: string, done?: boolean}) {
+        return this.http.put<any>(`${this.apiUrl}/${task.id}`, task).pipe(
+            tap(() => {
+                this.taskListSubject.next();
+            })
+        );
+    }
+
+    deleteTask(id: number) {
+        return this.http.delete<any>(`${this.apiUrl}/${id}`).pipe(
+            tap(() => {
+                this.taskListSubject.next();
+            })
+        )
+    }
+}
