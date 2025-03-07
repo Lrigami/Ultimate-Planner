a7bc5b3d1dc0426d434005eecac0d877d28a2520
Lrigami <prune.thiry@hotmail.com>
2025-03-07 12:42:21 +0100

    feat(task-card): first version of task-card

--- frontend/src/app/components/cards/task-card/task-card.component.ts
+++ frontend/src/app/components/cards/task-card/task-card.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../buttons/button.component';
 
 @Component({
-  selector: 'app-task-card',
-  imports: [],
+  selector: 'task-card',
+  imports: [FormsModule, ButtonComponent],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.css'
 })
 export class TaskCardComponent {
+  @Input() task!: { id: number, title: string, description?: string, kanban_category?: string, due_date?: Date };
+  titre: string = "card test";
+  description?: string;
+  kanban_category?: string = "to-do";
+  due_date?: Date;
+  tag?: string = "tag-test";
 
+  
 }
