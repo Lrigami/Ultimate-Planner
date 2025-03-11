3e732c17bcfa04908205d3030788a2591353123f
Lrigami <prune.thiry@hotmail.com>
2025-03-11 12:20:20 +0100

    feat(tag): tag form management

--- frontend/src/app/components/forms/tag-form/tag-form.component.ts
+++ frontend/src/app/components/forms/tag-form/tag-form.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ButtonComponent } from '../../buttons/button.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-tag-form',
-  imports: [],
+  selector: 'tag-form',
+  imports: [ButtonComponent],
   templateUrl: './tag-form.component.html',
   styleUrl: './tag-form.component.css'
 })
 export class TagFormComponent {
+  @Output() saveData = new EventEmitter<boolean>();
 
+  tagForm = new FormGroup({
+    tagname: new FormControl('', Validators.required),
+  })
+
+  onCloseForm(dataResponse: boolean) {
+    // Est-ce que je ne devrais pas gérer l'enregistrement du nouveau tag ici ?
+    this.saveData.emit(dataResponse);
+  }
 }
--- frontend/src/app/components/forms/task-form/task-form.component.ts
+++ frontend/src/app/components/forms/task-form/task-form.component.ts
@@ -1,17 +1,19 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { ButtonComponent } from '../../buttons/button.component';
 import { SaveFormComponent } from '../save-form/save-form.component';
+import { TagFormComponent } from '../tag-form/tag-form.component';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'task-form',
-  imports: [ButtonComponent, SaveFormComponent],
+  imports: [ButtonComponent, SaveFormComponent, TagFormComponent],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
 export class TaskFormComponent {
   @Output() isFormVisible = new EventEmitter<any>();
   isSaveFormVisible = false;
+  isTagFormVisible = false;
 
   taskForm = new FormGroup({
     title: new FormControl('', Validators.required), // Champ obligatoire
@@ -23,13 +25,20 @@ export class TaskFormComponent {
   }
 
   openNewTagForm() {
-    
+    this.isTagFormVisible = true;
   }
 
   handleSaveFormClose(dataResponse: boolean) {
     if (dataResponse) {
-      // gérer l'enregistrement des données
+      // gérer l'enregistrement des données de la tâche
     }
     this.isSaveFormVisible = false;
+  }
+
+  handleTagFormClose(dataResponse: boolean) {
+    if (dataResponse) {
+      // gérer l'enregistrement du nouveau tag
+    }
+    this.isTagFormVisible = false;
   }
 }
