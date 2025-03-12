57315889539f02e4a0731b6792af43f4abf6095c
Lrigami <prune.thiry@hotmail.com>
2025-03-12 14:48:09 +0100

    feat(task-list): listen changes on cards

--- frontend/src/app/components/task-views/task-list/task-list.component.ts
+++ frontend/src/app/components/task-views/task-list/task-list.component.ts
@@ -17,6 +17,9 @@ export class TaskListComponent {
 
   ngOnInit() {
     this.displayAll();
+    this.taskService.taskList$.subscribe(() => {
+      this.displayAll();
+    });
   }
 
   displayAll() {
