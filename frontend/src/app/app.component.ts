ff5bb059751d47260cfd928774b3ea2118a6bd1c
Lrigami <prune.thiry@hotmail.com>
2025-03-18 16:55:14 +0100

    style(list): test to-do-list component in app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ToDoListsComponent } from './pages/to-do-lists/to-do-lists.component';
 import { ToDoListComponent } from './pages/to-do-list/to-do-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ToDoListComponent],
+  imports: [CommonModule, RouterOutlet, ToDoListsComponent, ToDoListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
