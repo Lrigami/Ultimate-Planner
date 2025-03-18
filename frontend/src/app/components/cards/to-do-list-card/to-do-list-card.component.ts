a1ae0823c66a41dd676a3e9404efb9d7cd3ed626
Lrigami <prune.thiry@hotmail.com>
2025-03-18 16:54:53 +0100

    feat(list-card): inject list data in card

--- frontend/src/app/components/cards/to-do-list-card/to-do-list-card.component.ts
+++ frontend/src/app/components/cards/to-do-list-card/to-do-list-card.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Todolist } from '../../../models/todolist.model';
+import { TodolistService } from '../../../services/to-do-list.service';
+import { ButtonComponent } from '../../buttons/button.component';
 
 @Component({
-  selector: 'app-to-do-list-card',
-  imports: [],
+  selector: 'to-do-list-card',
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './to-do-list-card.component.html',
   styleUrl: './to-do-list-card.component.css'
 })
 export class ToDoListCardComponent {
+  // Input pour passer des informations du parent vers l'enfant : de to-do-list-list à to-do-list-card ici
+  @Input() list!: Todolist;
+  @Output() edit = new EventEmitter<Todolist>();
+  @Output() delete = new EventEmitter<Todolist>();
 
+  title?: string; 
+  pinned?: boolean; 
+  color?: string;
+
+  constructor(private todolistService: TodolistService) {}
+
+  ngOnInit() {
+    this.updateList();
+  }
+
+  updateList() {
+    console.log(this.list);
+    this.title = this.list.title;
+  }
 }
