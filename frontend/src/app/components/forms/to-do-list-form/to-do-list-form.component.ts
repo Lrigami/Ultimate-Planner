ea67983a96155473cb245f2725797bf7bf559e73
Lrigami <prune.thiry@hotmail.com>
2025-03-18 16:57:00 +0100

    refactor: add necessary component files for project

---
+++ frontend/src/app/components/forms/to-do-list-form/to-do-list-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list-form',
+  imports: [],
+  templateUrl: './to-do-list-form.component.html',
+  styleUrl: './to-do-list-form.component.css'
+})
+export class ToDoListFormComponent {
+
+}
