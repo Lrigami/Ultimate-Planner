1e234eec2afaced2d0ac71b735fc900d8c0962f9
Lrigami <prune.thiry@hotmail.com>
2025-03-18 15:51:59 +0100

    feat(to-do-list): to do list services link to backend

--- frontend/src/app/services/to-do-list.service.ts
+++ frontend/src/app/services/to-do-list.service.ts
@@ -5,30 +5,46 @@ import { Observable, tap, BehaviorSubject } from 'rxjs';
 @Injectable({
     providedIn: 'root',
 })
-export class TaskService {
+export class TodolistService {
     private todolistSubject = new BehaviorSubject<void>(null!);
     todolistList$ = this.todolistSubject.asObservable(); // Pour la liste des to-do-lists plus tard
-    private apiUrl = 'http://localhost:3000/todolists';
+    private apiUrl = 'http://localhost:3000/todolist';
 
     constructor (private http: HttpClient) {}
 
-    getAllLists() {
-
+    getAllLists(): Observable<any> {
+        return this.http.get<any>(`${this.apiUrl}`).pipe(
+            tap(() => {
+                this.todolistSubject.next();
+            })
+        )
     }
 
-    getList() {
-
+    getList(id: number): Observable<any> {
+        return this.http.get<any>(`${this.apiUrl}/${id}`).pipe(
+            tap(() => {
+                this.todolistSubject.next();
+            })
+        )
     }
 
-    createList() {
-
+    createList(list: {title: string, pinned: boolean, color: string}): Observable<any> {
+        return this.http.post<any>(this.apiUrl, list);
     }
 
-    updateList() {
-
+    updateList(list: {id: number, title: string, pinned: boolean, color: string}): Observable<any> {
+        return this.http.put<any>(`${this.apiUrl}/${list.id}`, list).pipe(
+            tap(() => {
+                this.todolistSubject.next();
+            })
+        )
     }
 
-    deleteList() {
-
+    deleteList(id: number): Observable<any> {
+        return this.http.delete<any>(`${this.apiUrl}/${id}`).pipe(
+            tap(() => {
+                this.todolistSubject.next();
+            })
+        )
     }
 }
