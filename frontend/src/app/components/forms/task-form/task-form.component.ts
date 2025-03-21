5898a5beb0b281abbad9ef852242770d2c6ed6bf
Lrigami <prune.thiry@hotmail.com>
2025-03-21 17:05:22 +0100

    fix(close): when task-form close is clicked, it closes without save-form if no modification was made (#19)

--- frontend/src/app/components/forms/task-form/task-form.component.ts
+++ frontend/src/app/components/forms/task-form/task-form.component.ts
@@ -58,7 +58,7 @@ export class TaskFormComponent implements OnInit, OnChanges {
         description: this.taskData.description ?? '',
         priority: this.taskData.priority ?? 'undefined',
         kanban_category: this.taskData.kanban_category ?? 'to-do',
-        due_date: this.taskData.due_date ? new Date(this.taskData.due_date).toISOString().split('T')[0] : '',
+        due_date: this.taskData.due_date ? new Date(this.taskData.due_date).toLocaleDateString('fr-CA') : '',
         isChecked: this.taskData.done
       });
     } else if (this.kanbanData) {
@@ -73,7 +73,19 @@ export class TaskFormComponent implements OnInit, OnChanges {
   }
 
   openSaveForm() {
-    this.isSaveFormVisible = true;
+    if (this.taskData) {
+      if (this.taskData.title === this.taskForm.value.title && (this.taskData.description === this.taskForm.value.description || !this.taskData.description) && new Date(this.taskData.due_date ? this.taskData.due_date : '').toDateString() === new Date(this.taskForm.value.due_date).toDateString() && this.taskData.priority === this.taskForm.value.priority && this.taskData.kanban_category === this.taskForm.value.kanban_category && this.taskData.done === this.taskForm.value.isChecked) {
+        this.closeForm();
+      } else {
+        this.isSaveFormVisible = true;
+      }
+    } else {
+      if (!this.taskForm.value.title && !this.taskForm.value.description) {
+        this.closeForm();
+      } else {
+        this.isSaveFormVisible = true;
+      }
+    }
   }
 
   openNewTagForm() {
