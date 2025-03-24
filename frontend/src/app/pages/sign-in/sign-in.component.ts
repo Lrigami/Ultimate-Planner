0b8302dbae2d6048b846c4e8fda8982f77e898e6
Lrigami <prune.thiry@hotmail.com>
2025-03-24 15:13:00 +0100

    feat(sign-in): add sign-in form

--- frontend/src/app/pages/sign-in/sign-in.component.ts
+++ frontend/src/app/pages/sign-in/sign-in.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/buttons/button.component';
+import { SignInFormComponent } from '../../components/forms/sign-in-form/sign-in-form.component';
 
 @Component({
-  selector: 'app-sign-in',
-  imports: [],
+  selector: 'sign-in',
+  imports: [ButtonComponent, SignInFormComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
