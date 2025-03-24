71b8056a127974ce19b8c18d85759d771aca1a08
Lrigami <prune.thiry@hotmail.com>
2025-03-24 16:46:58 +0100

    feat: sign-in and sign-up form are done

--- frontend/src/app/components/forms/sign-up-form/sign-up-form.component.ts
+++ frontend/src/app/components/forms/sign-up-form/sign-up-form.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import { ButtonComponent } from '../../buttons/button.component';
 
 @Component({
-  selector: 'app-sign-up-form',
-  imports: [],
+  selector: 'sign-up-form',
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ButtonComponent],
   templateUrl: './sign-up-form.component.html',
   styleUrl: './sign-up-form.component.css'
 })
 export class SignUpFormComponent {
+  isCapsLockOn = false;
+  isFirstPasswordVisible = false;
+  isSecondPasswordVisible = false;
 
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [Validators.required]);
+
+  isInvalid(): boolean {
+    return this.emailFormControl.invalid && (this.emailFormControl.dirty || this.emailFormControl.touched);
+  }
+
+  togglePasswordVisibility(password: string): void {
+    password === 'first' ? this.isFirstPasswordVisible = !this.isFirstPasswordVisible : this.isSecondPasswordVisible = !this.isSecondPasswordVisible;
+  }
+
+  onKeyDown(event: KeyboardEvent): void {
+    this.isCapsLockOn = event.getModifierState('CapsLock');
+  }
 }
--- frontend/src/app/pages/sign-in/sign-in.component.ts
+++ frontend/src/app/pages/sign-in/sign-in.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/buttons/button.component';
 import { SignInFormComponent } from '../../components/forms/sign-in-form/sign-in-form.component';
+import { SignUpFormComponent } from '../../components/forms/sign-up-form/sign-up-form.component';
 
 @Component({
   selector: 'sign-in',
-  imports: [ButtonComponent, SignInFormComponent],
+  imports: [ButtonComponent, SignInFormComponent, SignUpFormComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  goToSignUp() {
+    window.scrollBy({ left: window.innerWidth * 0.5, behavior: 'smooth' });
+  }
 
+  goToSignIn() {
+    window.scrollBy({ left: -window.innerWidth * 0.5, behavior: 'smooth' });
+  }
 }
