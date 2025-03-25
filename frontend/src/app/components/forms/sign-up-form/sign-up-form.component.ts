a5f084d1ceacda878ea1bf32f34d881988c1a6aa
Lrigami <prune.thiry@hotmail.com>
2025-03-25 11:55:09 +0100

    feat: passwords verifciations

--- frontend/src/app/components/forms/sign-up-form/sign-up-form.component.ts
+++ frontend/src/app/components/forms/sign-up-form/sign-up-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import { FormsModule, FormControl, ReactiveFormsModule, Validators, AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 import { ButtonComponent } from '../../buttons/button.component';
 
 @Component({
@@ -14,8 +14,45 @@ export class SignUpFormComponent {
   isFirstPasswordVisible = false;
   isSecondPasswordVisible = false;
 
+  passwordValidator(minLength: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = control.value;
+      
+      if (password && password.length < minLength) {
+        return { 'passwordTooShort': true };
+      }
+  
+      if (password && !/[A-Z]/.test(password)) {
+        return { 'missingUppercase': true };
+      }
+  
+      if (password && !/\d/.test(password)) {
+        return { 'missingNumber': true };
+      }
+  
+      if (password && !/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+        return { 'missingSpecialChar': true };
+      }
+  
+      return null;
+    };
+  }
+
+  passwordConfirmationValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = control.value;
+
+      if (password !== this.passwordFormControl.value) {
+        return { 'nonMatchingPassword': true};
+      }
+
+      return null;
+    };
+  }
+
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  passwordFormControl = new FormControl('', [Validators.required]);
+  passwordFormControl = new FormControl('', [Validators.required, this.passwordValidator(8)]);
+  passwordConfirmationFormControl = new FormControl('', [Validators.required, this.passwordConfirmationValidator()]);
 
   isInvalid(): boolean {
     return this.emailFormControl.invalid && (this.emailFormControl.dirty || this.emailFormControl.touched);
