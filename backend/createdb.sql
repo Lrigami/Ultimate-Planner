e44361d4aefc15bcbd046a3c72a35fbcc664685f
Lrigami <prune.thiry@hotmail.com>
2025-03-28 11:23:00 +0100

    feat: automation of database creation (#26)

---
+++ backend/createdb.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE ultimate_planner
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'fr-FR'
+    LC_CTYPE = 'fr-FR'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
