84a255b2a226e7bdbbf7da8896ab6a77d71db403
Lrigami <prune.thiry@hotmail.com>
2025-04-01 11:38:42 +0200

    style: adapt style now there's a menu

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
 import { AuthInterceptor } from './auth.interceptor';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
-    { provide: AuthInterceptor, useClass: AuthInterceptor, multi: true }
+    { provide: AuthInterceptor, useClass: AuthInterceptor, multi: true }, 
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' }}
   ],
 };
